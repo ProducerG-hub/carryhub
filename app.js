9e472d1047a344b4e8efa7310cd28755151bb44f
gwamaka mwakabuta <gwamakamwakabuta@gmail.com>
2026-06-05 12:59:53 +0300

    Authentication: Created the routes + logics to handle authentication processes (registering, logging in, logging out and viewing profile) and the logics works well since are tested in postman and approved them

--- app.js
+++ app.js
@@ -3,11 +3,34 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const pool = require('./config/db');
+const session = require('express-session');
+const router = require('./routes/urls');
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }, // Set to true if using HTTPS
+    cookie: { maxAge: 24 * 60 * 60 * 1000 } // 1 day validity
+}));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to CarryHub Backend!');
+});
+
+// graceful shutdown
+process.on('SIGINT', () => {
+    console.log('Shutting down server...');
+    pool.end(() => {
+        console.log('Database connection closed.');
+        process.exit(0);
+    }
+    );
 });
 
 app.listen(port, () => {
---
+++ controllers/authController.js
@@ -0,0 +1,75 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const pool = require('../config/db');
+
+module.exports.Register = async (req, res) => {
+    const { full_name, email, password_hash, phone } = req.body;
+    
+    if (!full_name || !email || !password_hash || !phone) {
+        return res.status(400).json({ success: false, message: 'All fields are required' });
+    }
+
+    try{
+        const existingUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (existingUser.rows.length > 0) {
+            return res.status(400).json({ success: false, message: 'Email already exists' });
+        }
+
+        const saltRounds = parseInt(process.env.SALT_ROUNDS) || 10;
+        const hashedPassword = await bcrypt.hash(password_hash, saltRounds);
+        const result = await pool.query('INSERT INTO users (full_name, email, password_hash, phone) VALUES ($1, $2, $3, $4)', [full_name, email, hashedPassword, phone]);
+
+        res.status(201).json({ success: true, message: 'User registered successfully' });
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+};
+
+module.exports.Login = async (req, res) => {
+    const { email, password_hash } = req.body;
+    if (!email || !password_hash) {
+        return res.status(400).json({ success: false, message: 'All fields are required' });
+    }
+
+    try {
+        const userResult = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (userResult.rows.length === 0) {
+            return res.status(400).json({ success: false, message: 'Bad credentials' });
+        }
+        const user = userResult.rows[0];
+        const passwordMatch = await bcrypt.compare(password_hash, user.password_hash);
+        if (!passwordMatch) {
+            return res.status(400).json({ success: false, message: 'Bad credentials' });
+        }
+        //user session data
+        req.session.user = {
+            id: user.user_id,
+            full_name: user.full_name,
+            email: user.email,
+            phone: user.phone
+        };
+        res.status(200).json({ success: true, message: 'Login successful' });
+    } catch (error) {
+        console.error('Error logging in:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+};
+
+module.exports.Logout = (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('Error logging out:', err);
+            return res.status(500).json({ success: false, message: 'Internal server error' });
+        }
+        res.clearCookie('connect.sid');
+        res.status(200).json({ success: true, message: 'Logout successful' });
+    });
+}
+
+module.exports.userProfile = (req, res) => {
+    if (!req.session.user) {
+        return res.status(401).json({ success: false, message: 'Unauthorized' });
+    }
+    res.status(200).json({ success: true, user: req.session.user });
+}
