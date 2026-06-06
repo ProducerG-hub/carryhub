2dbb72211db1d99c2453d132e9ac3704a171e0f7
gwamaka mwakabuta <gwamakamwakabuta@gmail.com>
2026-06-06 20:16:29 +0300

    Adding categories management endpoints including viewing the categories available, view by id and adding the category for admin

--- routes/urls.js
+++ routes/urls.js
@@ -1,11 +1,18 @@
 const authController = require('../controllers/authController');
+const categoryController = require('../controllers/categoryController');
 const authMiddleware = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
 
+// Authentication routes
 router.post('/api/auth/register', authController.Register);
 router.post('/api/auth/login', authController.Login);
 router.post('/api/auth/logout', authController.Logout);
 router.get('/api/auth/profile', authMiddleware.isAuthenticated, authController.userProfile);
+
+//category routes
+router.get('/api/categories', categoryController.getCategories);
+router.get('/api/categories/:id', categoryController.getCategoryById);
+router.post('/api/add-categories', authMiddleware.isAuthenticated, categoryController.addCategory);
 
 module.exports = router;
